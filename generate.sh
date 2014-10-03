bdee10b697b3234d5388f09b5727e3b74845108f
Eon S. Jeon <esjeon@hyunmu.am>
2014-10-03 17:33:09 +0900

    generate random number of commits per day

--- generate.sh
+++ generate.sh
@@ -3,14 +3,19 @@
 set -e
 
 days=$1
+rmin=${RANDMIN:-1}
+rmax=${RANDMIN:-5}
 
 rm -rf .git
 git init
 
 for i in $( seq 0 -1 -${days} ); do
-	time=$(date --date="$i day" "+%s %z")
-	echo "${time}" > time
-	git add time
-	git commit --date="${time}" -m "${time}"
+	rand=$(( RANDOM % ( rmax - rmin ) + rmin ))
+	for j in $( seq -1 -1 -${rand} ); do
+		time=$(date --date="$i day $j hour" "+%s %z")
+		echo "${time}" > time
+		git add time
+		git commit --date="${time}" -m "${time}"
+	done
 done
 
